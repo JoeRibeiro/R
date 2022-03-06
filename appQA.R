326bd5ae3caccd1106707e53012dfbae932e3774
Joe Ribeiro <joseph.ribeiro@cefas.co.uk>
2022-03-06 14:30:35 +0000

    Finished this qa comparison file

--- appQA.R
+++ appQA.R
@@ -1,12 +1,13 @@
 
 MAINDIR<- "C:/Users/JR13/OneDrive - CEFAS/Fish_dataproduct_QSR_JR/"
 RDIR<- paste0(MAINDIR,"R/")
-QADIR_pre_update<- paste0(MAINDIR,"app_QA_downloaded_from_zapp/pre_update/")
+QADIR_pre_update<- paste0(MAINDIR,"app_QA_downloaded_from_zapp/preupdated/")
 QADIR_update<- paste0(MAINDIR,"app_QA_downloaded_from_zapp/updated/")
+outdir<- paste0(MAINDIR,"app_QA_downloaded_from_zapp/")
 
 checklist_pre = list.files(QADIR_pre_update,include.dirs=T,full.names = T)
 checklist_post = list.files(QADIR_update,include.dirs=T,full.names = T)
-outdf = data.frame(matrix(ncol=5,nrow=0, dimnames=list(NULL, c("Survey","EEZgroup","EEZJointRegime","ZAPercentBeforeUpdate","ZAPercentAfterUpdate"))))
+outdf = data.frame(matrix(ncol=7,nrow=0, dimnames=list(NULL, c("speciesarea","Survey","EEZgroup","EEZJointRegime","Year","ZAPercentBeforeUpdate","ZAPercentAfterUpdate"))))
 
 for(combrow in 1:length(checklist_pre)){
   prefile=read.csv(checklist_pre[combrow])
@@ -19,9 +20,17 @@ for(combrow in 1:length(checklist_pre)){
   postfile$link=paste0(postfile$Year,postfile$EEZ.or.joint.regime,postfile$Survey,postfile$EEZ.group)
   
   combfiles=merge(prefile,postfile,by='link',suffixes = c("2021","2022"))
-  combfiles = combfiles[,c("Survey2022","EEZ.group2022","EEZ.or.joint.regime2022","ZA..2021","ZA..2022")]
-  colnames(combfiles) = c("Survey","EEZgroup","EEZJointRegime","ZAPercentBeforeUpdate","ZAPercentAfterUpdate")
+  arealst = strsplit(checklist_post[combrow],"/");   arealst = arealst[[1]][length(arealst[[1]])]
+  areai = substr(arealst,27,nchar(arealst)-4)
+  combfiles$speciesarea = areai
+
+  combfiles = combfiles[,c("speciesarea","Survey2022","Year2022","EEZ.group2022","EEZ.or.joint.regime2022","ZA..2021","ZA..2022")]
+  colnames(combfiles) = c("speciesarea","Survey","Year","EEZgroup","EEZJointRegime","ZAPercentBeforeUpdate","ZAPercentAfterUpdate")
+
   outdf=rbind(outdf,combfiles)
 }
-  
-  
+
+outdf <- outdf[order(outdf$speciesarea,outdf$Survey,outdf$EEZgroup,outdf$EEZJointRegime,outdf$Year),]
+
+outdf$percent_difference = 100*(outdf$ZAPercentAfterUpdate/outdf$ZAPercentBeforeUpdate)-100
+write.csv(outdf,paste0(outdir,"differences_caused_by_update.csv"),row.names = F)
