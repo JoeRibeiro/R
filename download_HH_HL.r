15099a844e0aacfaf7a02d95a56a48ea60565659
Joe Ribeiro <joseph.ribeiro@cefas.co.uk>
2022-05-23 15:29:01 +0100

    Changes were made in the last 20 minutes. THIS IS AN AUTOMATED COMMIT EVERY 20 MINUTES

--- download_HH_HL.r
+++ download_HH_HL.r
@@ -1,4 +1,6 @@
 rm(list=ls()) #clear environment, start afresh
+MAINDIR<- "C:/Users/JR13/OneDrive - CEFAS/Fish_dataproduct_QSR_JR/"
+
 
 #useful packages
 library("icesDatras")
@@ -6,22 +8,23 @@ years <- 1991:2011
 quarters <- 1:4
 
 # Not an ideal approach for downloading the data but the library currently only supports one quarter/year at a time https://github.com/ices-tools-prod/icesDatras/issues/35
-i=-1
 for(survey in getSurveyList()){
+  i=-1
   for(year in years){
     for(quarter in quarters){
-    dl <- try(getHHdata(survey,years,quarters); i=i+1)
+        skip_to_next <- FALSE
+        # Note that print(b) fails since b doesn't exist
+        tryCatch(dl <- getHHdata(survey,year,quarter), error = function(e) { skip_to_next <<- TRUE})
+        if(skip_to_next) { next } else { 
+          i=i+1 
+          if(i==0){ #first file
+            HH = dl} else { # append if not first successful download
+              HH = rbind(HH,dl)
+            }
+        }
+    }
+  }
+  write.csv(HH,paste0(MAINDIR,"HH_HL_download/HH/HH-",survey,".csv"))
 }
-
-
-
-survey_Q_C_S_combinations<-read.csv("R/survey_Q_C_S_combinations.csv")# for QSR
-
-
-MAINDIR<- "C:/Users/JR13/OneDrive - CEFAS/Fish_dataproduct_QSR_JR/"
-RDIR<- paste0(MAINDIR,"R/")
-HHDIR<- paste0(MAINDIR,"HH_HL_download/HH/")
-HLDIR<- paste0(MAINDIR,"HH_HL_download/HL/")
-
 
 
