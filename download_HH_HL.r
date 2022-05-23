15ce0e7554f64e156c0b88c2766b592d60eed81d
Joe Ribeiro <joseph.ribeiro@cefas.co.uk>
2022-05-23 15:09:01 +0100

    Changes were made in the last 20 minutes. THIS IS AN AUTOMATED COMMIT EVERY 20 MINUTES

--- download_HH_HL.r
+++ download_HH_HL.r
@@ -2,12 +2,17 @@ rm(list=ls()) #clear environment, start afresh
 
 #useful packages
 library("icesDatras")
+years <- 1991:2011
+quarters <- 1:4
 
-survey <- "NS-IBTS"
-year <- 2019
-quarter <- 1
+# Not an ideal approach for downloading the data but the library currently only supports one quarter/year at a time https://github.com/ices-tools-prod/icesDatras/issues/35
+i=-1
+for(survey in getSurveyList()){
+  for(year in years){
+    for(quarter in quarters){
+    dl <- try(getHHdata(survey,years,quarters); i=i+1)
+}
 
-HH <- getHHdata(survey) 
 
 
 survey_Q_C_S_combinations<-read.csv("R/survey_Q_C_S_combinations.csv")# for QSR
